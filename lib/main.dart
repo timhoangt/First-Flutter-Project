ede7934cdab73d2dcf029a8664fd68700c4fa5b3
Timoth Hoang <timothy.hoang1@marist.edu>
2020-05-28 15:00:12 -0400

    added card and image

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Food Paradise')
+        ],),),
       ),
     );
   }
